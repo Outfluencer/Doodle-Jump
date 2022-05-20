7720fadf5e903ad961f60910c23cab446aeab9dd
BuildTools <unconfigured@null.spigotmc.org>
2022-05-20 21:03:07 +0200

    added ColorChanger.java

--- src/main/java/xyz/doodlejump/mysql/Communication.java
+++ src/main/java/xyz/doodlejump/mysql/Communication.java
@@ -4,11 +4,13 @@ public class Communication {
 
 
     public static boolean tryLogin(String username, String password) {
+        if(username.isEmpty() || password.isEmpty()) return false;
         return true;
     }
 
 
     public static boolean tryRegister(String username, String password) {
+        if(username.isEmpty() || password.isEmpty()) return false;
         return true;
     }
 
